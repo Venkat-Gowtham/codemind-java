40eb2ceb9600f105dbe3887970c2d17855b46c8d
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:48:49 +0530

    import java.util.*; import java.lang.Math.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         int b=a.nextInt();         int r=(b*(b-1))/2;         System.out.println(r);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        int r=(b*(b-1))/2;
+        System.out.println(r);
+    }
+}
