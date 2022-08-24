bfdc8375962d524b684beb706fa37ffc8f21112e
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 12:55:51 +0530

    import java.util.*; class gowtham {     public static void main(String args[])     {         Scanner mu=new  Scanner(System.in);         float a=mu.nextFloat();         float b=mu.nextFloat();         float c;         c=a*b;         System.out.printf("%.2f",c);
        }
            
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class gowtham
+{
+    public static void main(String args[])
+    {
+        Scanner mu=new  Scanner(System.in);
+        float a=mu.nextFloat();
+        float b=mu.nextFloat();
+        float c;
+        c=a*b;
+        System.out.printf("%.2f",c);
+        
+    }
+        
+}
