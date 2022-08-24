2c7ea4265c0328cf2a9a0aa04c1dd32d077f558e
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:46:23 +0530

    import java.util.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         String b=a.nextLine();         System.out.println(b);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        String b=a.nextLine();
+        System.out.println(b);
+    }
+}
