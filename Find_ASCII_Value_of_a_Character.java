df6fc44a059646aea882cab97f3149456ad6bb3f
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:07:34 +0530

    import java.util.*; class gowtham {     public static void main(String args[])     {         Scanner mu = new  Scanner(System.in);        char a=mu.next().charAt(0);        int c=a;         System.out.println(c);
        }
            
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class gowtham
+{
+    public static void main(String args[])
+    {
+        Scanner mu = new  Scanner(System.in);
+       char a=mu.next().charAt(0);
+       int c=a;
+        System.out.println(c);
+        
+    }
+        
+}
