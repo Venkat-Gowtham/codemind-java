2d2eeb12e93b5a74682ecbb799193995610e195c
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:41:13 +0530

    import java.util.*; class disc {     public static void main(String args[] )     {         Scanner ka=new Scanner(System.in);         int a=ka.nextInt();         int b=ka.nextInt();         int c=ka.nextInt();         int r=2*a*b*c*512;         System.out.println(r);     }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class disc
+{
+    public static void main(String args[] )
+    {
+        Scanner ka=new Scanner(System.in);
+        int a=ka.nextInt();
+        int b=ka.nextInt();
+        int c=ka.nextInt();
+        int r=2*a*b*c*512;
+        System.out.println(r);
+    }
+    
+}
