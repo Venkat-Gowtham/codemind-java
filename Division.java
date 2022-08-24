5d61eef0b9b3d981abd57f727e02275e671d48fa
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:42:55 +0530

    import java.util.*; import java.lang.Math.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         int b=a.nextInt();         int c=a.nextInt();         int r=b/c;         System.out.println(r);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        int c=a.nextInt();
+        int r=b/c;
+        System.out.println(r);
+    }
+}
