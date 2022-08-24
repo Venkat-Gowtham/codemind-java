6a6d94151ca7080fb5fa01ad23f3549e7f048a17
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 15:02:02 +0530

    import java.util.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         int b=a.nextInt();         if(b%2==0)         {             System.out.println("Even");         }         else         {           System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        if(b%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+          System.out.println("Odd");   
+        }
+    }
+}
