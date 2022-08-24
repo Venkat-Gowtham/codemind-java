5d0ed4b506e3416e359b84d112b4e6b881384206
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 14:01:17 +0530

    import java.util.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         int b=a.nextInt();         double r=32+(1.8*b);         System.out.printf("%.2f",r);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        double r=32+(1.8*b);
+        System.out.printf("%.2f",r);
+    }
+}
