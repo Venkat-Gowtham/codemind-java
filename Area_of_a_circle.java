ecd28f3f10a350cb74b382900ccb4758808a2d23
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-08-24 13:33:48 +0530

    import java.util.*; import java.lang.Math.*; class g {     public static void main(String args[])     {         Scanner a=new Scanner(System.in);         int b=a.nextInt();         final double pi=3.14;         double r=(b*b)*pi;         System.out.println(String.format("%.2f",r));     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        final double pi=3.14;
+        double r=(b*b)*pi;
+        System.out.println(String.format("%.2f",r));
+    }
+}
