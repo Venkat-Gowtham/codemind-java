cc5003a3407dfd5fd36cdb7d75b4f27075f29e3b
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-09-06 19:37:10 +0530

    import java.util.*; class g {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int s=0;         for(int i=1;i<a;i++)         {             if(a%i==0)             {                 s+=i;             }         }         if(s>=a)         {          System.out.println("True");         }         else         {             System.out.println("False");         }
        }
        
    }

---
+++ Abundant_Numbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class g
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int s=0;
+        for(int i=1;i<a;i++)
+        {
+            if(a%i==0)
+            {
+                s+=i;
+            }
+        }
+        if(s>=a)
+        {
+         System.out.println("True");   
+        }
+        else
+        {
+            System.out.println("False");
+        }
+        
+    }
+    
+}
