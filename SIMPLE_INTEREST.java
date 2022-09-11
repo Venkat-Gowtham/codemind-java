b6edf8401692027a9dbbf80bfc9db2cb0269a610
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-09-11 11:08:55 +0530

    import java.util.*; class f {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int q=sc.nextInt();         int r=sc.nextInt();        int s=(int)((p*q*r)/100);         System.out.println(s);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class f
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        int r=sc.nextInt();
+       int s=(int)((p*q*r)/100);
+        System.out.println(s);
+    }
+}
